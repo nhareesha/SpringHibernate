a73d89fe60b9098dbc4b4af266016ac07ba4eead
hareesha <hareesha1906@gmail.com>
2016-08-21 23:56:57 -0700

    Add request bean

---
+++ orm/src/main/java/orm/beans/PersonBean.java
@@ -0,0 +1,42 @@
+package orm.beans;
+
+public class PersonBean {
+	
+	private String fname;
+	private String lname;
+	private double salary;
+	private String empType;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getEmpType() {
+		return empType;
+	}
+
+	public void setEmpType(String empType) {
+		this.empType = empType;
+	}
+
+}
